89f857f7cd5b6785c1b0ef0a5270b9e2afb1b73c
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-04-26 23:48:09 -0300

    alterações

--- Challenges/JS/Beginner/1001 - Extremely Basic/extremely_basic.js
+++ Challenges/JS/Beginner/1001 - Extremely Basic/extremely_basic.js
@@ -1,11 +1,11 @@
 const readline = require("readline");
 
 const cosulta = readline.createInterface({
-    input: process.stdin, output: process.stdout
+    input: process.stdin, 
+    output: process.stdout
 });
     
 cosulta.on("line", (n1) => {
-
     cosulta.on("line", (n2) => {
         console.log("X = " + (Number(n1) + Number(n2)));
         
